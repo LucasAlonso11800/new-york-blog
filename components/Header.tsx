24ce1564fe12a045a4ce8550b99bb947c5b59ceb
Lucas <lucasalonso11800@gmail.com>
2021-12-23 13:52:27 -0300

    Created Head and basic Header components

---
+++ components/Header.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import classes from '../styles/components/Header.module.css';
+import Link from 'next/link';
+
+export default function Header() {
+    const [width, setWidth] = useState<number>();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [query, setQuery] = useState('');
+
+    if (typeof window !== 'undefined') window.addEventListener('resize', () => setWidth(window.innerWidth));
+
+    useEffect(() => setWidth(window.innerWidth), []);
+
+    // Make search work
+    // Get categories and URLs from backend
+    // Get image from backend
+
+    return (
+        <header className={classes.header}>
+            <nav className={classes.nav}>
+                <ul className={classes.list}>
+                    {width && width > 765 ?
+                        <li className={classes.listItem}>
+                            <a className={classes.link}>Categories</a>
+                            <ul className={classes.sublist}>
+                                <li className={classes.listItem}><Link href="/living">NYC Life</Link></li>
+                                <li className={classes.listItem}><Link href="/travel" >Travel</Link></li>
+                                <li className={classes.listItem}><Link href="/guides" >Guides</Link></li>
+                                <li className={classes.listItem}><Link href="/food" >Food and Drink</Link></li>
+                                <li className={classes.listItem}><Link href="/art-and-culture" >Art and Culture</Link></li>
+                                <li className={classes.listItem}><Link href="/out-and-about" >Out and About</Link></li>
+                                <li className={classes.listItem}><Link href="/style" >Style</Link></li>
+                            </ul>
+                        </li>
+                        :
+                        <li className={classes.listItem}>
+                            <a className={classes.link} onClick={() => setMenuOpen(!menuOpen)}>Menu</a>
+                            {menuOpen &&
+                                <ul className={`${classes.sublist} ${classes.open}`}>
+                                    <li className={classes.listItem}><Link href="/living">NYC Life</Link></li>
+                                    <li className={classes.listItem}><Link href="/travel" >Travel</Link></li>
+                                    <li className={classes.listItem}><Link href="/guides" >Guides</Link></li>
+                                    <li className={classes.listItem}><Link href="/food" >Food and Drink</Link></li>
+                                    <li className={classes.listItem}><Link href="/art-and-culture" >Art and Culture</Link></li>
+                                    <li className={classes.listItem}><Link href="/out-and-about" >Out and About</Link></li>
+                                    <li className={classes.listItem}><Link href="/style" >Style</Link></li>
+                                </ul>
+                            }
+                        </li>
+                    }
+                </ul>
+                <form>
+                    <input
+                        className={classes.input}
+                        type="search"
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                        placeholder='Search'
+                    />
+                </form>
+            </nav>
+            <div className={classes.siteLogoContainer}>
+                <p className={classes.siteLogo}>
+                    <Link href="/">.</Link>
+                </p>
+            </div>
+        </header>
+    )
+};
