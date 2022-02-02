31220f04d751255201d840818285625aa026fc6d
Lucas <lucasalonso11800@gmail.com>
2022-02-02 18:39:46 -0300

    New url change

--- const/ServerURL.ts
+++ const/ServerURL.ts
@@ -1,1 +1,4 @@
-export const SERVER_URL: string = '/api/graphql';
+export const SERVER_URL: string = typeof location !== 'undefined' ?
+    `${location.protocol}//${location.hostname}:${location.port}/api/graphql` :
+    '/api/graphql'
+    ;
