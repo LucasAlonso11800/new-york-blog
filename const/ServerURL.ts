06c7b9526e37a0147427389343236ae7e67e23b9
Lucas <lucasalonso11800@gmail.com>
2022-02-02 18:44:59 -0300

    Weird change

--- const/ServerURL.ts
+++ const/ServerURL.ts
@@ -1,4 +1,4 @@
 export const SERVER_URL: string = typeof location !== 'undefined' ?
     `${location.protocol}//${location.hostname}:${location.port}/api/graphql` :
-    '/api/graphql'
+    'https://lucas-alonso-social-media.herokuapp.com/graphql'
     ;
